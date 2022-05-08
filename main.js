212d5826cd6742a64aabbe80ad4b1e5ef9029616
Tomasz Tchorzewski <tomasz.tchorzewski@me.com>
2022-05-08 12:10:05 +0200

    prework-zad19 start

---
+++ main.js
@@ -0,0 +1,8 @@
+let firstNumber = 1;
+let secondNumber = 2;
+
+function add(firstNumber, secondNumber){
+    let result = firstNumber + secondNumber;
+    return result;
+}
+console.log(add(firstNumber, secondNumber));
